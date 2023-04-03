97a427b7d5c759e342fbb8feba21a0de35a21bc3
a19meram <a19meram@student.his.se>
2023-04-03 02:49:03 +0200

    Adding sendRequest function
    Adding sendRequest function with an ajax call to the database connection file

--- MySQL/fetchData.js
+++ MySQL/fetchData.js
@@ -1,7 +1,19 @@
 function getData() {
     var county = $("#county").val();
-    var iterations = 0; 
+    var iterations = 0;
     var totalElapsedTime = 0;
     var iterationCount = 0;
-    var timeData = ''; 
+    var timeData = '';
+
+    function sendRequest() {
+        $.ajax({
+            url: "Mysql_DB_Connection.php?county=" + county,
+            dataType: "json",
+            success: function (data) {
+                $("#lan-rubrik").html("Data för: " + county);
+                $("#myDiv").css("display", "block");
+            }
+
+        });
+    }
 }
