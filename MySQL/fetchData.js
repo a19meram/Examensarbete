2be3175728df30e28212f1beccde5a52d5b5b5d5
a19meram <a19meram@student.his.se>
2023-04-03 02:36:13 +0200

    Change iterations
    iteration shall be 0 from the beginning

--- MySQL/fetchData.js
+++ MySQL/fetchData.js
@@ -1,6 +1,6 @@
 function getData() {
     var county = $("#county").val();
-    var iterations = 10; 
+    var iterations = 0; 
     var totalElapsedTime = 0;
     var iterationCount = 0;
     var timeData = ''; 
