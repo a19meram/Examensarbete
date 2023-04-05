1854ff9273792413392d96930f1bfe7b490c2b87
a19meram <a19meram@student.his.se>
2023-04-05 07:38:32 +0200

    Build query in mongodb

--- MongoDB/Mongo_DB_Connection.php
+++ MongoDB/Mongo_DB_Connection.php
@@ -10,4 +10,31 @@ $county = $_GET['county'];
 // Select the database and collection
 $database = $client->selectDatabase('MongoDB_EX');
 $collection = $database->selectCollection('fordonSkuld');
+
+// Build the query
+$filter = [];
+if ($county == "alla") {
+    $filter = [];
+} elseif ($county != "") {
+    $filter['County'] = $county;
+}
+
+$options = [
+    'projection' => [
+        'Year' => 1,
+        'debtType' => 1,
+        'Amount' => 1,
+        '_id' => 0
+    ]
+];
+
+// Find documents matching the query
+$documents = $collection->find($filter, $options);
+
+// Convert the results to an array
+$data = iterator_to_array($documents);
+
+// Send the results as JSON
+header('Content-Type: application/json');
+echo json_encode($data);
 ?>
