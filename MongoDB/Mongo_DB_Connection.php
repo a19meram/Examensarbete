32330e7bfab39b3171045433ca987189cd79fce3
a19meram <a19meram@student.his.se>
2023-04-05 07:37:32 +0200

    Select database
    Select the database and collection in MongoDB

--- MongoDB/Mongo_DB_Connection.php
+++ MongoDB/Mongo_DB_Connection.php
@@ -4,4 +4,10 @@ require 'vendor/autoload.php';
 // Create a MongoDB client
 $client = new MongoDB\Client("mongodb://localhost:27017");
 
+// Get the county from the query string
+$county = $_GET['county'];
+
+// Select the database and collection
+$database = $client->selectDatabase('MongoDB_EX');
+$collection = $database->selectCollection('fordonSkuld');
 ?>
