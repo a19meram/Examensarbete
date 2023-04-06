25d6655a133a7c387df3491859533ff5170423f6
a19meram <a19meram@student.his.se>
2023-04-06 11:51:28 +0200

    sendRequest function

--- MongoDB/fetchData.js
+++ MongoDB/fetchData.js
@@ -5,6 +5,21 @@ function getData() {
     var totalElapsedTime = 0;
     var iterationCount = 0;
     var timeData = '';
+
+    function sendRequest() {
+        $.ajax({
+            url: 'Mongo_DB_Connection.php',
+            type: 'GET',
+            dataType: 'json',
+            data: { county: county },
+            success: function (data) {
+                $("#lan-rubrik").html("Data för: " + county);
+                $("#lan-rubrik2").html("Data för: " + county);
+                togellfun(data);
+            }
+        });
+    }
+    sendRequest();
 }
 
 function togellfun(data) {
