a5e1e2fa84fde3bd5e5c639db5a182bd55376b17
a19meram <a19meram@student.his.se>
2023-04-06 11:46:26 +0200

    adding variables

--- MongoDB/fetchData.js
+++ MongoDB/fetchData.js
@@ -1,5 +1,10 @@
+var myChart, myChart2;
 function getData() {
-
+    var county = $("#county").val();
+    var iterations = 0;
+    var totalElapsedTime = 0;
+    var iterationCount = 0;
+    var timeData = '';
 }
 
 function togellfun(data) {
